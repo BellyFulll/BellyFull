81367ec948d69c32f55c8d97cfa8b2c18e6949a0
teojiesern <teojiesern42@gmail.com>
2023-11-05 13:59:45 +0800

    testing code owner

--- app/src/main/java/com/example/bellyfull/PregnancyAdviceAndTipsActivity.java
+++ app/src/main/java/com/example/bellyfull/PregnancyAdviceAndTipsActivity.java
@@ -1,4 +1,10 @@
 package com.example.bellyfull;
 
 public class PregnancyAdviceAndTipsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("Hello World!");
+    }
 }
