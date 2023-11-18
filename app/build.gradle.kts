ed6d5ccf6313bdb9fcbf72b691b8d1b229d2bdce
JC Chuan <jc.chuan.0303@gmail.com>
2023-11-18 23:54:07 +0800

    add sample database code, fix dependancies left out

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,8 @@ dependencies {
     // firebase
     implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
 
     // Exclude kotlin-stdlib-jdk8
     configurations.implementation {
