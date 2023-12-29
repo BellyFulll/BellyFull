190aa346fe1db5eaaf152d13033167a9370630e1
JC Chuan <jc.chuan.0303@gmail.com>
2023-12-29 18:37:54 +0800

    config file stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,6 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.1.4" apply false
+    id("com.android.application") version "8.2.0" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
 }
