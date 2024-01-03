b1b0eecb1c1eb690c4d17d180a87c45cf56ad570
JC Chuan <jc.chuan.0303@gmail.com>
2024-01-03 23:23:26 +0800

    exit dialog

--- app/src/main/java/com/example/bellyfull/MainActivity.java
+++ app/src/main/java/com/example/bellyfull/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.bellyfull;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,11 +8,15 @@ import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    private AlertDialog alertDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,16 +26,45 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = navHostFragment.getNavController();
         setupBottomNavMenu(navController);
     }
+        @Override
+    public void onBackPressed() {
+        showExitConfirmationDialog();
+    }
 
     private void setupBottomNavMenu(NavController navController) {
         BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
         NavigationUI.setupWithNavController(bottomNav, navController);
     }
 
-    @Override
-    public void onBackPressed() {
-        // Finish the current activity and all activities below it in the stack
-        finishAffinity();
-        Log.i(TAG, "finishAffinity() called");
+
+    private void showExitConfirmationDialog() {
+        // Create a custom dialog with your layout
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View dialogView = getLayoutInflater().inflate(R.layout.exit_dialog, null);
+        builder.setView(dialogView);
+
+        Button confirmButton = dialogView.findViewById(R.id.confirmButton);
+        Button cancelButton = dialogView.findViewById(R.id.cancelButton);
+        // Set click listeners for buttons
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Finish the current activity and all activities below it in the stack
+                finishAffinity();
+                Log.i(TAG, "finishAffinity() called");
+                alertDialog.dismiss();
+            }
+        });
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // User clicked the "No" button, dismiss the dialog
+                alertDialog.dismiss();
+            }
+        });
+
+        // Create and show the AlertDialog
+        alertDialog = builder.create();
+        alertDialog.show();
     }
 }
