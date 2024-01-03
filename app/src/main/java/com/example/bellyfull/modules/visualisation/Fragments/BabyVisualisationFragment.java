e45b3ab7ae3b58afb77d444e42703e891ddee6de
teojiesern <teojiesern42@gmail.com>
2024-01-03 23:43:05 +0800

    add visual placeholder for visualisation module

---
+++ app/src/main/java/com/example/bellyfull/modules/visualisation/Fragments/BabyVisualisationFragment.java
@@ -0,0 +1,11 @@
+package com.example.bellyfull.modules.visualisation.Fragments;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.bellyfull.R;
+
+public class BabyVisualisationFragment extends Fragment {
+    public BabyVisualisationFragment() {
+        super(R.layout.fragment_baby_visualisation);
+    }
+}
---
+++ app/src/main/java/com/example/bellyfull/modules/visualisation/Fragments/MomVisualisationFragment.java
@@ -0,0 +1,11 @@
+package com.example.bellyfull.modules.visualisation.Fragments;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.bellyfull.R;
+
+public class MomVisualisationFragment extends Fragment {
+    public MomVisualisationFragment() {
+        super(R.layout.fragment_mom_visualisation);
+    }
+}
