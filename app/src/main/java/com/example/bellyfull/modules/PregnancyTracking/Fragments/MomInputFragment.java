8642c563e4dcb4a0288b26031157f03f0c4135cd
teojiesern <teojiesern42@gmail.com>
2024-01-05 01:37:37 +0800

    fix back button not pop fragment

--- app/src/main/java/com/example/bellyfull/modules/PregnancyTracking/Fragments/MomInputFragment.java
+++ app/src/main/java/com/example/bellyfull/modules/PregnancyTracking/Fragments/MomInputFragment.java
@@ -2,10 +2,13 @@ package com.example.bellyfull.modules.PregnancyTracking.Fragments;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import com.example.bellyfull.R;
 
@@ -18,5 +21,15 @@ public class MomInputFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        Button btnMomCancel = view.findViewById(R.id.btnMomCancel);
+
+
+        btnMomCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavDirections action = MomInputFragmentDirections.actionMomInputFragmentToHomeFragment();
+                Navigation.findNavController(view).navigate(action);
+            }
+        });
     }
 }
