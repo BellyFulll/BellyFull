8d5f0f5110115451ff55d0d87a4b8ea9abe0faea
Jun Xian <junxian@Juns-MacBook-Pro-3.local>
2024-01-08 11:20:06 +0800

    visualisation frontent for baby growth

---
+++ app/src/main/java/com/example/bellyfull/modules/visualisation/Fragments/WHOgraphicFragment.java
@@ -0,0 +1,7 @@
+package com.example.bellyfull.modules.visualisation.Fragments;
+
+import androidx.fragment.app.Fragment;
+import com.example.bellyfull.R;
+public class WHOgraphicFragment extends Fragment {
+    public WHOgraphicFragment() {super(R.layout.fragment_who_graphic); };
+}
