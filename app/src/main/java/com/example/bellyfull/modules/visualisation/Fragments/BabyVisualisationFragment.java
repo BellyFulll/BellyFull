1318923707bd9536104d369213f7302d8931ec61
Jun Xian <junxian@Juns-MacBook-Pro-3.local>
2024-01-10 13:02:59 +0800

    babyvisualisation frontend revamp

--- app/src/main/java/com/example/bellyfull/modules/visualisation/Fragments/BabyVisualisationFragment.java
+++ app/src/main/java/com/example/bellyfull/modules/visualisation/Fragments/BabyVisualisationFragment.java
@@ -1,113 +1,68 @@
 package com.example.bellyfull.modules.visualisation.Fragments;
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.bellyfull.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
-import java.util.ArrayList;
 public class BabyVisualisationFragment extends Fragment {
-
-    private RecyclerView recyclerView;
-    private ArrayList<Baby> babyArrayList;
-    private MyAdapter myAdapter;
-    private FirebaseFirestore db;
-    private ProgressDialog progressDialog;
-    private Spinner spinner; //Declare at outside
+    public BabyVisualisationFragment() {
+        super(R.layout.fragment_baby_visualisation);
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_baby_visualisation, container, false);
 
-        progressDialog = new ProgressDialog(requireContext());
-        progressDialog.setCancelable(false);
-        progressDialog.setMessage("Fetching data...");
-        progressDialog.show();
+        TabLayout tabLayout = view.findViewById(R.id.tab_layout);
+        ViewPager2 viewPager2 = view.findViewById(R.id.graphic_viewpager);
 
-        recyclerView = view.findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
+        BabyPagerAdapter adapter = new BabyPagerAdapter(this);
+        viewPager2.setAdapter(adapter);
 
-        // Use LinearLayoutManager with reverseLayout set to true
-        LinearLayoutManager layoutManager = new LinearLayoutManager(requireContext());
-        layoutManager.setReverseLayout(true);
-        layoutManager.setStackFromEnd(true); // Optional, ensures that the last item stays at the bottom
-        recyclerView.setLayoutManager(layoutManager);
+        final String[] tabTitles = {"Graphic WHO", "Graphic CDC"};
 
-        db = FirebaseFirestore.getInstance();
-        babyArrayList = new ArrayList<>();
-        myAdapter = new MyAdapter(requireContext(), babyArrayList);
-
-        recyclerView.setAdapter(myAdapter);
-        eventChangeListener();
-
-        spinner = view.findViewById(R.id.spinner);
-
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-                requireContext(), R.array.spinner_items, android.R.layout.simple_spinner_item);
-
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-
-        FloatingActionButton btnBabyInput = view.findViewById(R.id.btnBabyInput);
-        btnBabyInput.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Handle FAB click
-                navigateToBabyInputFragment();
-            }
-        });
+        new TabLayoutMediator(tabLayout, viewPager2,
+                (tab, position) -> tab.setText(tabTitles[position])
+        ).attach();
 
         return view;
     }
 
-    private void eventChangeListener() {
-        db.collection("BABY_INFO")
-                .orderBy("timestamp", Query.Direction.DESCENDING) // Assuming you have a timestamp field for sorting
-                .addSnapshotListener((value, error) -> {
-                    if (error != null) {
-                        Log.e("Firestore error", "Error getting documents: ", error);
-                        return;
-                    }
+    private class BabyPagerAdapter extends FragmentStateAdapter {
+        private final int NUM_PAGES = 2;
+        BabyPagerAdapter(Fragment fragment) {
+            super(fragment);
+        }
 
-                    if (value != null) {
-                        babyArrayList.clear(); // Clear existing data
-                        for (DocumentSnapshot document : value) {
-                            babyArrayList.add(0, document.toObject(Baby.class)); // Add new items at the beginning
-                        }
-                        myAdapter.notifyDataSetChanged();
-                    }
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+           switch(position) {
+               case 0:
+                   return new WHOgraphicFragment();
+               case 1:
+                   return new CDCgraphicFragment();
+               default:
+                   return new WHOgraphicFragment();
+           }
+        }
 
-                    if (progressDialog.isShowing()) {
-                        progressDialog.dismiss();
-                    }
-                });
-    }
-
-    private void navigateToBabyInputFragment() {
-        // Use Navigation Component to navigate to BabyInputFragment
-        NavHostFragment.findNavController(this)
-                .navigate(R.id.action_babyVisualisationFragment_to_babyInputFragment);
+        @Override
+        public int getItemCount() {
+            return NUM_PAGES;
+        }
     }
 }
