0d5384e1796ccaaaffd77abfd7c869c7bbe4b583
teojiesern <teojiesern42@gmail.com>
2024-01-14 22:07:06 +0800

    refactor mom visualisation ui

--- app/src/main/java/com/example/bellyfull/modules/PersonalisedNotifications/NotificationReceiver.java
+++ app/src/main/java/com/example/bellyfull/modules/PersonalisedNotifications/NotificationReceiver.java
@@ -1,20 +1,15 @@
 package com.example.bellyfull.modules.PersonalisedNotifications;
 
 import android.app.Notification;
-import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
 
 import com.example.bellyfull.Constant.notification_constant;
 import com.example.bellyfull.R;
-
-import java.math.BigInteger;
-import java.util.UUID;
 
 public class NotificationReceiver extends BroadcastReceiver {
     public static int notificationId = 0;
@@ -24,7 +19,7 @@ public class NotificationReceiver extends BroadcastReceiver {
         String title = intent.getStringExtra("title");
         String message = intent.getStringExtra("message");
         Notification notification = new NotificationCompat.Builder(context, notification_constant.EVENT_CHANNEL_ID)
-                .setSmallIcon(R.drawable.momcta)
+                .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle(title)
                 .setContentText(message)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT).build();
